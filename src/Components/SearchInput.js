36d27dfc6b3f07cf82f6f317ae97d1b6fd998faf
pavangajula <pavan.gajula@copart.com>
2022-05-02 18:19:24 +0530

    configured redux toolkit

--- src/Components/SearchInput.js
+++ src/Components/SearchInput.js
@@ -2,16 +2,17 @@ import React, { useRef } from "react";
 import "./SearchInput.css";
 
 const SearchInput = () => {
-
-    const searchValue = useRef();
-  return <section className="section search">
-         <form className="search-form">
-           <div className="form-control">
-            <label htmlFor="name">Search Cocktail</label>
-            <input type="text" name="name" id="name" ref={searchValue}/>
-           </div>
-         </form>
-  </section>;
+  const searchValue = useRef();
+  return (
+    <section className="section search">
+      <form className="search-form">
+        <div className="form-control">
+          <label htmlFor="name">Search Cocktail</label>
+          <input type="text" name="name" id="name" ref={searchValue} />
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default SearchInput;
