9f3e72354760e1c216a277ffcd5d63bd8c30cf9b
pavangajula <pavan.gajula@copart.com>
2022-05-04 18:29:03 +0530

    displaying the singlecocklist information

--- src/Redux/features/cocktailSlice.js
+++ src/Redux/features/cocktailSlice.js
@@ -9,6 +9,15 @@ export const fetchCocktails = createAsyncThunk(
   }
 );
 
+export const fetchSingleCocktail = createAsyncThunk(
+  "cocktails/fetchSingleCocktail",
+  async ({ id }) => {
+    return fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+    ).then((res) => res.json());
+  }
+);
+
 const cocktailSlice = createSlice({
   name: "cocktails",
   initialState: {
@@ -26,6 +35,17 @@ const cocktailSlice = createSlice({
       state.cocktails = action.payload.drinks;
     },
     [fetchCocktails.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    [fetchSingleCocktail.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [fetchSingleCocktail.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.cocktail = action.payload.drinks;
+    },
+    [fetchSingleCocktail.rejected]: (state, action) => {
       state.loading = false;
       state.error = action.payload;
     },
